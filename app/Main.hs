b87ed92aa4806b6533930a00682755b9cbbc4325
Tomoyuki Ueda <tu@formalism.net>
2016-11-23 21:12:19 +0900

    Implement streaming of input query result.

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import System.IO                        (stdout)
+import System.IO                        (stdout,stdin)
 import Control.Monad.IO.Class           (liftIO)
 import qualified Web.Authenticate.OAuth as OA
 import qualified Data.Conduit.List      as CL
@@ -14,18 +14,20 @@ oauth = OA.newOAuth {
     OA.oauthRequestUri      = "https://api.twitter.com/oauth/request_token",
     OA.oauthAccessTokenUri  = "https://api.twitter.com/oauth/access_token",
     OA.oauthAuthorizeUri    = "https://api.twitter.com/oauth/authorize",
-    OA.oauthConsumerKey     = "your consuer key here",
-    OA.oauthConsumerSecret  = "your consumer secret here" 
+    OA.oauthConsumerKey     = "your consumer key here",
+    OA.oauthConsumerSecret  = "your consumer secret here"
 }
 
 credential = OA.newCredential "your access token" "your access token secret"
 
 main :: IO ()
 main = do
+    S.hPutStr stdout "Enter some keyword to search in tweets: "
+    key <- S.hGetLine stdin
     manager <- HC.newManager HC.defaultManagerSettings
-    request <- HC.parseRequest "POST https://api.twitter.com/1.1/statuses/update.json"
-    let postRequest = HC.urlEncodedBody [("status", "Test from Haskell")] request
-    signedRequest <- OA.signOAuth oauth credential postRequest
+    request <- HC.parseRequest "GET https://stream.twitter.com/1.1/statuses/filter.json"
+    let req = HC.setQueryString [("track", Just key)] request
+    signedRequest <- OA.signOAuth oauth credential req
     HS.httpSink signedRequest $ \res -> do
         liftIO $ Prelude.putStrLn $ "Status code:" ++ show (HS.getResponseStatusCode res)
         CL.mapM_ (S.hPut stdout)
